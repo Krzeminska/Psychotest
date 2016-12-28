262d32c41462dc997e5e0383e9a6f36e5508d3da
Wsuwka <katsarnam@gmail.com>
2016-12-28 17:35:11 +0100

    START

---
+++ src/PsychoProfile.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wsuwka
+ */
+public class PsychoProfile {
+    float IE; //if(-IE) Introwertk; else if(IE) Ekstrawertyk;
+    float No;
+    float FT;
+    float JI;
+    
+    PsychoProfile(){
+        IE=0;
+        No=0;
+        FT=0;
+        JI=0;
+    }
+}
